f160c6a9d908a3c22dc65bf70fca0cb0b1ed4ef8
EderZerlin <eder.zerlin@gmail.com>
2020-05-16 11:29:36 -0300

    2 files modified

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+//Importacoes necessarias para formularios
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+/*
+* Para funcionar os formularios, precisamos importar (adicionar)
+* O modulo ReactiveFormsModule no arquivo .module.ts
+*/
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
